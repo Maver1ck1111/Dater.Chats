25e3204f70d243549abe5dac6c65fc567993aa6f
Maver1ck1111 <borisovd885@gmail.com>
2025-07-23 15:05:35 +0300

    added entities for chat

---
+++ Chats.Domain/Chats.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chats.Domain
+{
+    public class Chat
+    {
+        [BsonId]
+        [BsonGuidRepresentation(GuidRepresentation.Standard)]
+        public Guid ID { get; set; }
+
+        [BsonElement("usersId")]
+        public List<Guid> UsersId { get; set; } = new List<Guid>();
+
+        [BsonElement("messages")]
+        public List<Message> Messages { get; set; } = new List<Message>();
+    }
+}
---
+++ Chats.Domain/Message.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chats.Domain
+{
+    public class Message
+    {
+        [BsonId]
+        [BsonGuidRepresentation(GuidRepresentation.Standard)]
+        public Guid ID { get; set; }
+
+        [BsonElement("senderId")]
+        public Guid SenderId { get; set; }
+
+        [BsonElement("text")]
+        public string Text { get; set; } = string.Empty;
+
+        [BsonElement("timestamp")]
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
